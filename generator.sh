e00d78811d2e8d1d0fe1f9872353bfe4c0511e20
Ivan Dudin <s@vandud.ru>
2020-02-24 19:45:31 +0300

    fix error in date

--- generator.sh
+++ generator.sh
@@ -6,7 +6,7 @@ cd ~/cleaning-plan.vandud.beget.tech/public_html;
 
 touch `pwd`/previous.html;
 > `pwd`/previous.html;
-cat `pwd`/header.prev | sed -e "s#Date#Для периода с <b>$(date -d "`date +%Y-%m-%d` - `expr $(date +%u) - 1 - 7` days" +%d-%m-%Y)</b> по <b>$(date -d "`date +%Y-%m-%d` - `expr $(date +%u) - 1 - 7` days + 7 days" +%d-%m-%Y)</b>#g" >> `pwd`/previous.html;
+cat `pwd`/header.prev | sed -e "s#Date#Для периода с <b>$(date -d "`date +%Y-%m-%d` - `expr $(date +%u) - 1` days - 7 days" +%d-%m-%Y)</b> по <b>$(date -d "`date +%Y-%m-%d` - `expr $(date +%u) - 1` days" +%d-%m-%Y)</b>#g" >> `pwd`/previous.html;
 
 Location_nums=(`for i in {0..2}; do expr \( \( \( $(date +%W) % 3 \) + $i \) % 3 \) + 1; done`);
 
@@ -52,7 +52,7 @@ cat `pwd`/footer >> `pwd`/index.html;
 
 touch `pwd`/next.html;
 > `pwd`/next.html;
-cat `pwd`/header.next | sed -e "s#Date#Для периода с <b>$(date -d "`date +%Y-%m-%d` - `expr $(date +%u) - 1 + 7` days" +%d-%m-%Y)</b> по <b>$(date -d "`date +%Y-%m-%d` - `expr $(date +%u) - 1 + 7` days + 7 days" +%d-%m-%Y)</b>#g" >> `pwd`/next.html;
+cat `pwd`/header.next | sed -e "s#Date#Для периода с <b>$(date -d "`date +%Y-%m-%d` - `expr $(date +%u) - 1` days + 7 days" +%d-%m-%Y)</b> по <b>$(date -d "`date +%Y-%m-%d` - `expr $(date +%u) - 1` days + 14 days" +%d-%m-%Y)</b>#g" >> `pwd`/next.html;
 
 Location_nums=(`for i in {0..2}; do expr \( \( \( $(date +%W) % 3 \) + $i \) % 3 \) + 1; done`);
 
